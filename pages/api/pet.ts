61a20145ec62f3a1e65bed5d10828eeebf97783e
Irena Georgieva <irenayordgeorgieva@gmail.com>
2020-10-26 14:18:49 +0200

    Resolves #4

--- pages/api/pet.ts
+++ pages/api/pet.ts
@@ -2,20 +2,27 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import db from '../../lib/db'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  if (req.method === 'GET') {
+    return new Promise((resolve) => {
+      db.collection('pets').get()
+        .then(snapshot => snapshot.docs.map(doc => doc.data()))
+        .then((pets) => {
+          if (pets.length === 0) {
+            res.status(204).end()
+          } else {
+            res.status(200).json(pets)
+          }
+          resolve()
+        })
+        .catch((err) => {
+          res.status(500).json(err)
+          resolve()
+        })
+    })
+  }
+
   return new Promise((resolve) => {
-    db.collection('pets').get()
-      .then(snapshot => snapshot.docs.map(doc => doc.data()))
-      .then((pets) => {
-        if (pets.length === 0) {
-          res.status(204).end()
-        } else {
-          res.status(200).json(pets)
-        }
-        resolve()
-      })
-      .catch((err) => {
-        res.status(405).json(err)
-        resolve()
-      })
+    res.status(405).end()
+    resolve()
   })
 }
